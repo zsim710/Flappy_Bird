f064eb889acec0d9ef526ab20af44a6d83d2f69d
zsim710 <zsim710@aucklanduni.ac.nz>
2024-05-09 13:00:27 +1200

    pipe modified for signed
    changed to signed, can go into negative, bit sizes should be suitable.

--- FPGA Testing/VGA_Bouncing_Ball_Test_2/MiniprojectResources/pipe_(first try).vhd
+++ FPGA Testing/VGA_Bouncing_Ball_Test_2/MiniprojectResources/pipe_(first try).vhd
@@ -9,48 +9,43 @@ ENTITY pipe IS
   PORT (
     clk, vert_sync : IN std_logic;
     pixel_row, pixel_column : IN std_logic_vector(9 DOWNTO 0);
-    red, green, blue : OUT std_logic
+    pipe_on : OUT std_logic
   );
 END pipe;
 
 ARCHITECTURE behavior OF pipe IS
-  SIGNAL pipe_x_pos : std_logic_vector(9 DOWNTO 0);
+  SIGNAL pipe_x_pos : std_logic_vector(10 DOWNTO 0);
   SIGNAL pipe_width, pipe_height : std_logic_vector(9 DOWNTO 0);
   SIGNAL screen_width, screen_height : std_logic_vector(9 DOWNTO 0);
-  SIGNAL pipe_on : std_logic;
 
 BEGIN
   -- Screen and pipe dimensions
-  screen_width  <= "1010000000"; -- 640 in binary
-  screen_height <= "111100000";  -- 480 in binary
-  pipe_width    <= "1100100";    -- 100 in binary
-  pipe_height   <= "100101100";  -- 300 in binary
+  screen_width  <= CONV_STD_LOGIC_VECTOR(640, 11);-- 640 in binary
+  screen_height <= CONV_STD_LOGIC_VECTOR(480, 11);  -- 480 in binary
+  pipe_width    <= CONV_STD_LOGIC_VECTOR(100, 10); -- 100 in binary
+  pipe_height   <= CONV_STD_LOGIC_VECTOR(300, 10); -- 300 in binary
 
   -- Initialize pipe starting position on the right side of the screen
-  pipe_x_pos    <= screen_width; -- Start from the far right
+  pipe_x_pos    <= screen_width + pipe_width; -- Start from the far right
 
   -- Process to move the pipe
   pipe_movement : PROCESS(vert_sync)
   BEGIN
-    IF rising_edge(clk) THEN
+    IF rising_edge(vert_sync) THEN
       IF pipe_x_pos <= "0000000000" THEN
         pipe_x_pos <= screen_width; -- Reset to the right side of the screen
       ELSE
-        pipe_x_pos <= pipe_x_pos - CONV_STD_LOGIC_VECTOR(1, 10); -- Move left by one pixel each clock cycle
+        pipe_x_pos <= pipe_x_pos - CONV_STD_LOGIC_VECTOR(2, 10); -- Move left by one pixel each clock cycle
       END IF;
     END IF;
   END PROCESS;
 
   -- Check if current pixel is within the bounds of the pipe
-  pipe_on <= '1' WHEN (pixel_column >= pipe_x_pos AND
-                        pixel_column <= pipe_x_pos + pipe_width - 1 AND
+  pipe_on <= '1' WHEN ('0' & pixel_column <= '0' & pipe_x_pos AND
+                        '0' & pixel_column >=  '0' & pipe_x_pos - pipe_width AND
                         pixel_row >= screen_height - pipe_height AND
-                        pixel_row <= screen_height - 1)
-                ELSE '0'; 
+                        pixel_row <= screen_height)
+                ELSE '0';
 
   -- Set the output colors, pipe in red, background in black
-  red   <= pipe_on;
-  green <= '0';
-  blue  <= '0';
-
 END behavior;
