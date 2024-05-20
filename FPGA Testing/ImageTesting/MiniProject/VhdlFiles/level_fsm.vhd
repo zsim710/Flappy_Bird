5c22d3b7e0c6a8ad4be8649792d4069c8e5ed9b9
zsim710 <zsim710@aucklanduni.ac.nz>
2024-05-20 16:07:25 +1200

    readable fsm only one pause state

--- FPGA Testing/ImageTesting/MiniProject/VhdlFiles/level_fsm.vhd
+++ FPGA Testing/ImageTesting/MiniProject/VhdlFiles/level_fsm.vhd
@@ -34,76 +34,36 @@ begin
     end if;
   end process;
 
-  NextState : process (state, witch0, witch1, left_click, right_click, back, collison_counter, pb0, pb3)
+  NextState : process (state, collison_counter, score)
   begin
 
     case (state) is
 
         -- state 0 --
-      when "0000" => -- menu state -> default state 
-        if (witch0 = '0' and left_click = '0' and witch1 = '1' and witch2 = '1') then -- sw = 1 = down = training
-          next_state <= "0001"; --state 1 training mode
-        elsif (witch1 = '0' and left_click = '0' and witch1 = '1' and witch2 = '1') then -- sw = 0 = up = normal
-          next_state <= "0010"; -- state 2 normal mode
-        elsif (witch2 = '0' and left_click = '0' and witch0 = '1' and witch1 = '1') then
-          next_state <= "0011"; -- s3
+      when  easy => -- menu state -> default state 
+        if (score >= 30) then -- sw = 1 = down = training
+          next_state <= medium; --state 1 training mode
         else
-          next_state <= "0000"; -- s0
+          next_state <= easy; -- s0
         end if;
 
         -- state 1 -- // training mode playing state
-      when "0001" => -- state 1 -> 0001
-        if (collison_counter >= 3) then
-          next_state <= "0110"; -- s6
-        elsif (right_click = '0') then
-          next_state <= "0100"; -- s4
+      when medium => -- state 1 -> 0001
+        if (score >=60) then -- sw = 0 = up = normal
+        next_state <= hard; -- state 2 normal mode
         else
-          next_state <= "0001"; -- s1
+          next_state <= medium; -- s1
         end if;
         -- state 2 --// normal mode playing state
-      when "0010" => -- state 2 --> 0010
-        if (collison_counter >= 1) then
-          next_state <= "0110"; -- s6
-        elsif (right_click = '0') then
-          next_state <= "0101"; -- s5
+      when hard => -- state 2 --> 0010
+        if(score >=120) then 
+          next_state <= impossible; -- state 3 settings mode
         else
-          next_state <= "0010"; -- s2
+          next_state <= hard; -- s2
         end if;
         -- state 3 -- // settings mode state
-      when "0011" => -- state 3 --> 0011
-        if (left_click = '0') then
-          next_state <= "0000"; -- s0 --> return to menu
-        end if;
-        -- state 4 --// pause training mode state
-      when "0100" => -- state 4 --> 0100
-        if (pb3 = '0') then
-          next_state <= "0001"; -- s1 --> return back to playing training mode
-        elsif (pb0 = '0') then
-          next_state <= "0000"; -- s0 --> return to menu
-        else
-          next_state <= "0100"; -- s4 --> stay in pause training mode
-
-        end if;
-        -- state 5 -- // pause normal mode state
-      when "0101" => -- state 5 --> 0101
-        if (pb3 = '0') then
-          next_state <= "0010"; -- s2 --> return back to playing normal mode
-        elsif (pb0= '0') then
-          next_state <= "0000"; -- s0 --> return to menu
-        else
-          next_state <= "0101"; -- s5 --> stay in pause normal mode
-        end if;
-        -- state 6 -- // end game state
-      when "0110" => -- state 6 --> 0110
-        if (right_click = '0') then -- go back to menu 
-          next_state <= "0000"; -- return to menu to start a new game 
-        elsif(witch1 = '0' and left_click = '0' and witch0 = '1') then -- play again to whatever mode they were in 
-          next_state <= "0010"; -- s2 --> go back to normal mode state
-        elsif(witch0 = '0' and left_click = '0' and witch1 = '1') then 
-          next_state <= "0001"; -- s1 --> go to back training mode 
-        end if;
-      when others =>
-        next_state <= "0000";
+      when impossible => -- state 3 --> 0011
+          next_state <= impossible; -- s0 --> return to menu
 
     end case;
 
@@ -112,32 +72,23 @@ begin
   output_state : process (state) -- output state
   begin
 
-    menu_state          <= '0'; -- menu state
-    training_mode       <= '0'; -- training mode
-    normal_mode         <= '0'; -- normal mode
-    settings_mode       <= '0'; -- settings mode
-    pause_training_mode <= '0'; -- pause training mode
-    pause_normal_mode   <= '0'; -- pause normal mode
-    end_game            <= '0'; -- end game
+    easy_mode_out       <= '0'; -- settings mode
+    medium_mode_out <= '0'; -- pause training mode
+    hard_mode_out   <= '0'; -- pause normal mode
+    impossible_mode_out            <= '0'; -- end game
 
     menu_state <= '0'; -- menu state
     case (state) is
-      when "0000" => -- menu state
-        menu_state <= '1';
-      when "0001" => -- training mode playing state
-        training_mode <= '1';
-      when "0010" => -- normal mode playing state
-        normal_mode <= '1';
-      when "0011" => -- settings mode state
-        settings_mode <= '1';
-      when "0100" => -- pause training mode state
-        pause_training_mode <= '1';
-      when "0101" => -- pause normal mode state
-        pause_normal_mode <= '1';
-      when "0110" => -- end game state
-        end_game <= '1';
+      when easy => -- menu state
+        easy_mode_out <= '1';
+      when medium => -- training mode playing state
+        medium_mode_out <= '1';
+      when hard => -- normal mode playing state
+        hard_mode_out <= '1';
+      when impossible=> -- settings mode state
+        impossible_mode_out <= '1';
       when others =>
-        menu_state <= '1';
+        easy_mode_out <= '1';
     end case;
   end process;
 
--- FPGA Testing/ImageTesting/MiniProject/VhdlFiles/main_fsm_readable.vhd
+++ FPGA Testing/ImageTesting/MiniProject/VhdlFiles/main_fsm_readable.vhd
@@ -6,121 +6,137 @@ use IEEE.STD_LOGIC_UNSIGNED.all;
 entity fsm is
   port
   (
-    pb2, clk, reset, right_click, back, pMenu, pb3 : in std_logic;
+    pb2, clk, reset, right_click, back, pb0, pb3 : in std_logic;
     collison_counter                        : in integer range 0 to 3;
-    witcMenu, witch1, witch2                          : in std_logic;
+    witch0, witch1, witch2                          : in std_logic;
     left_click                              : in std_logic;
     menu_state                              : out std_logic;
     training_mode                           : out std_logic;
     normal_mode                             : out std_logic;
     settings_mode                           : out std_logic;
-    pause_training_mode                     : out std_logic;
-    pause_normal_mode                       : out std_logic;
-    end_game                                : out std_logic
-  );
+    pause_game                    : out std_logic;
+    end_game                                : out std_logic);
 end fsm;
 
+-- states: 
+-- s0: menu state
+-- s1: training mode
+-- s2: normal mode
+-- s3: settings(not done)
+-- s4: pause training mode
+-- s5: pause normal mode 
+-- s6: end game 
 architecture Behavioral of fsm is
-  type state_type is (Menu, Training, Normal, Settings, pause_training, pause_normal, dead);
-  signal state      : state_type;
-  signal next_state : state_type;
+    type state_type is (Menu, Training, Normal, Settings, pause_game, dead);
+    signal state      : state_type;
+    signal next_state : state_type;
 
 begin
   sync : process (clk)
   begin
-    if (reset = Menu') then
-      state <= Menu;
+    if (reset = '0') then -- reset is SW[9]
+      state <= Menu; -- s0
     elsif (rising_edge(clk)) then
       state <= next_state;
     end if;
   end process;
+  
 
-  NextState : process (state, witcMenu, witch1, left_click, right_click, back, collison_counter, pMenu, pb3)
+  NextState : process (state, witch0, witch1, left_click, right_click, back, collison_counter, pb0, pb3)
   begin
+
+    
+
     case (state) is
-      when Menu =>
-        if (witcMenu = Menu' and left_click = Menu' and witch1 = '1' and witch2 = '1') then
-          next_state <= Training;
-        elsif (witch1 = Menu' and left_click = Menu' and witch1 = '1' and witch2 = '1') then
-          next_state <= Normal;
-        elsif (witch2 = Menu' and left_click = Menu' and witcMenu = '1' and witch1 = '1') then
-          next_state <= Settings;
+
+        -- state 0 --
+      when Menu => -- menu state -> default state 
+        if (witch0 = '0' and left_click = '0' and witch1 = '1' and witch2 = '1') then -- sw = 1 = down = training
+          next_state <= Training; --state 1 training mode
+        elsif (witch1 = '0' and left_click = '0' and witch1 = '1' and witch2 = '1') then -- sw = 0 = up = normal
+          next_state <= Normal; -- state 2 normal mode
+        elsif (witch2 = '0' and left_click = '0' and witch0 = '1' and witch1 = '1') then
+          next_state <= Settings; -- s3
         else
-          next_state <= Menu;
+          next_state <= Menu; -- s0
         end if;
-      when Training =>
+
+        -- state 1 -- // training mode playing state
+      when Training => -- state 1 -> 0001
         if (collison_counter >= 3) then
-          next_state <= dead;
-        elsif (right_click = Menu') then
-          next_state <= pause_training;
+          next_state <= "0110"; -- s6
+        elsif (right_click = '0') then
+          next_state <= pause_game; -- s4
         else
-          next_state <= Training;
+          next_state <= Training; -- s1
         end if;
-      when Normal =>
+        -- state 2 --// normal mode playing state
+      when Normal => -- state 2 --> 0010
         if (collison_counter >= 1) then
-          next_state <= dead;
-        elsif (right_click = Menu') then
-          next_state <= pause_normal;
+          next_state <= "0110"; -- s6
+        elsif (right_click = '0') then
+          next_state <= pause_game; -- s4
         else
-          next_state <= Normal;
+          next_state <= Normal; -- s2
         end if;
-      when Settings =>
-        if (left_click = Menu') then
-          next_state <= Menu;
+        -- state 3 -- // settings mode state
+      when Settings => -- state 3 --> 0011
+        if (left_click = '0') then
+          next_state <= Menu; -- s0 --> return to menu
         end if;
-      when pause_training =>
-        if (pb3 = Menu') then
-          next_state <= Training;
-        elsif (pMenu = Menu') then
-          next_state <= Menu;
+        -- state 4 --// pause training mode state
+      when pause_game => -- state 4 --> 0100
+        if (pb3 = '0' and witch0 = '0' and witch1 = '1' and witch2 = '1') then
+          next_state <= Training; -- s1 --> return back to playing training mode
+        elsif(pb3 = '0' and witch1 = '0' and witch0 = '1' and witch2 = '1') then
+          next_state <= Normal; -- s2 --> return back to playing normal mode
+        elsif (pb0 = '0') then
+          next_state <= Menu; -- s0 --> return to menu
         else
-          next_state <= pause_training;
+          next_state <= pause_game; -- s4 --> stay in pause training mode
+
         end if;
-      when pause_normal =>
-        if (pb3 = Menu') then
-          next_state <= Normal;
-        elsif (pMenu= Menu') then
-          next_state <= Menu;
-        else
-          next_state <= pause_normal;
-        end if;
-      when dead =>
-        if (right_click = Menu') then
-          next_state <= Menu;
-        elsif (witch1 = Menu' and left_click = Menu' and witcMenu = '1') then
-          next_state <= Normal;
-        elsif (witcMenu = Menu' and left_click = Menu' and witch1 = '1') then
-          next_state <= Training;
+        -- state 5 -- // pause normal mode state
+        -- state 6 -- // end game state
+      when dead => -- state 5 --> 0110
+        if (right_click = '0') then -- go back to menu 
+          next_state <= Menu; -- return to menu to start a new game 
+        elsif(witch1 = '0' and left_click = '0' and witch0 = '1') then -- play again to whatever mode they were in 
+          next_state <= Normal; -- s2 --> go back to normal mode state
+        elsif(witch0 = '0' and left_click = '0' and witch1 = '1') then 
+          next_state <= Training; -- s1 --> go to back training mode 
         end if;
       when others =>
         next_state <= Menu;
+
     end case;
+
   end process;
 
-  output_state : process (state)
+  output_state : process (state) -- output state
   begin
-    menu_state          <= Menu';
-    training_mode       <= Menu';
-    normal_mode         <= Menu';
-    settings_mode       <= Menu';
-    pause_training_mode <= Menu';
-    pause_normal_mode   <= Menu';
-    end_game            <= Menu';
 
+    menu_state          <= '0'; -- menu state
+    training_mode       <= '0'; -- training mode
+    normal_mode         <= '0'; -- normal mode
+    settings_mode       <= '0'; -- settings mode
+    pause_training_mode <= '0'; -- pause training mode
+    pause_normal_mode   <= '0'; -- pause normal mode
+    end_game            <= '0'; -- end game
+
+    menu_state <= '0'; -- menu state
     case (state) is
-      when Menu =>
+      when Menu => -- menu state
         menu_state <= '1';
-      when Training =>
+      when Training => -- training mode playing state
         training_mode <= '1';
-      when Normal =>
+      when Normal => -- normal mode playing state
         normal_mode <= '1';
-      when Settings =>
+      when Settings => -- settings mode state
         settings_mode <= '1';
-      when pause_training =>
+      when pause_game => -- pause training mode state
         pause_training_mode <= '1';
-      when pause_normal =>
-        pause_normal_mode <= '1';
-      when dead =>
+      when dead => -- end game state
         end_game <= '1';
       when others =>
         menu_state <= '1';
